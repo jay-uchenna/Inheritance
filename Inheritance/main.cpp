7cf0b9fbf5a103f0719eefd134ed205843c590f3
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2022-01-05 00:04:49 +0300

    Initial Commit

---
+++ Inheritance/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Inheritance
+//
+//  Created by Jedidiah Uchenna on 05.01.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
